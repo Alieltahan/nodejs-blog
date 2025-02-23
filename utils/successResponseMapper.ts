7a6786e761b6baf9a1976ba35c880a08ad15963e
Alieltahan <alyaltahan@gmail.com>
2025-02-23 16:21:31 +0200

    Fix type

--- utils/successResponseMapper.ts
+++ utils/successResponseMapper.ts
@@ -1,6 +1,6 @@
 import { HttpStatusCode } from "./constants";
 
-export function successResponseMapper(code: HttpStatusCode,response: object) {
+export function successResponseMapper(code: HttpStatusCode,response: object | object[]) {
 	return {
 		data: response,
 		status: 'success',
